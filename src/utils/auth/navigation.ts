fc60dbc7aa5c3de7f9b5ee2ae97ae93271301b1e
wangqing <tlxiaoyun@qq.com>
2026-09-10 09:39:18 +0800

    chore(auth): clean up unused openWebApp navigation helper (#16)

--- src/utils/auth/navigation.ts
+++ src/utils/auth/navigation.ts
@@ -3,7 +3,3 @@ import { env } from "@/env"
 export function openLogIn() {
   window.open(`${env.WXT_WEBSITE_URL}/log-in`, "_blank")
 }
-
-export function openWebApp() {
-  window.open(`${env.WXT_WEBSITE_URL}/home`, "_blank")
-}
